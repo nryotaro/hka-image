3d521772594ef22c4d2dfbec5995c3dac6a174e4
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2018-11-24 18:29:34 +0900

    fix patterns

--- ds_image/datasets.py
+++ ds_image/datasets.py
@@ -9,10 +9,10 @@ import ds_image.image as i
 exhaustive_xy = list(itertools.product(range(3, 8), range(3, 8)))
 
 #: Contains the hiragana characters.
-hiragana_list = [chr(i) for i in range(12353, 12436)] # ゔ
+hiragana_list = [chr(i) for i in range(12353, 12439)]
 
 #: Contains the katakana characters.
-katanaka_list = [chr(i) for i in range(12449, 12532+1)]
+katanaka_list = [chr(i) for i in range(12449, 12539)]
 
 #: Contains the alphabet characters.
 alphabet_list = [chr(i) for i in list(range(65, 65+26)) + list(range(97, 97+26))]
@@ -39,8 +39,9 @@ class DatasetsGenerator:
         Returns 
         -------
         list
-            Each item is the tuple that the first element is an image, 
-            and the second one is a one-hot vector that represents the writing system of the letter in the image.
+            Each item is the tuple that the first element is an image,
+            and the second one is a one-hot vector that represents
+            the writing system of the letter in the image.
         """
         return [(self._image_generator.generate(char, xy, size), label.one_hot()) \
                 for xy, size, label, char in self.generate_petterns()]
@@ -53,13 +54,22 @@ class DatasetsGenerator:
         list
             Each item is ((int, int), int, :py:class:`ds_image.label.Targets`, str).
         """
-        return [(xy, size, label, char) for xy, size, (label, char) in itertools.product(
-                exhaustive_xy,
-                sizes, 
+        size_font_patterns = itertools.product(sizes,
                 list(zip(itertools.repeat(l.Targets.HIRAGANA), hiragana_list)) +
                 list(zip(itertools.repeat(l.Targets.KATAKANA), katanaka_list)) +
-                list(zip(itertools.repeat(l.Targets.ALPHABET), alphabet_list)))]
+                list(zip(itertools.repeat(l.Targets.ALPHABET), alphabet_list)))
 
+
+        return [(xy, font_size, label, letter) for font_size, (label, letter) in size_font_patterns \
+                 for xy in self._calc_positions(font_size)]
+
+    def _calc_positions(self, size):
+        """
+        """
+        edge = ((28 - size) / 2) - 2
+        dots = [edge + padding for padding in range(0, 5)]
+        return [(x, y) for x, y in itertools.product(
+            dots, dots)]
 
 def create_datasets_generator():
     """Constructs :py:class:`DatasetsGenerator`
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,7 +1,8 @@
 """datasets
 """
 import pytest
-import ds_image.datasets as d 
+import ds_image.datasets as d
+import ds_image.label as l
 
 
 def test_exhaustive_xy():
@@ -13,25 +14,43 @@ def test_exhaustive_xy():
 def test_characters():
     """characters
     """
-    assert 'あ' in d.hiragana_list 
-    assert 'ア' in d.katanaka_list
-    assert 'A' in d.alphabet_list
-    assert 'a' in d.alphabet_list
+    for target in ['ゔ', 'ゃ', 'ゕ', 'ゖ']:
+        assert target in d.hiragana_list
+
+    for target in ['ヴ', 'ヵ', 'ヶ','ヷ', 'ヸ', 'ヹ', 'ヺ']:
+        assert target in d.katanaka_list
+
+    for target in ['A', 'a']:
+        assert target in d.alphabet_list
 
 
 class TestDatasetsGenerator:
     """DatasetsGenerator
     """
 
-
     def test_generate_patterns(self):
         """generate_patterns
         """
         datasets_generator = d.create_datasets_generator()
-        assert len(datasets_generator.generate_petterns()) == 16425
-    
+        patterns = datasets_generator.generate_petterns()
+
+        assert len(patterns) == 17100
+
+        for pattern in [((4,4), 16, l.Targets.ALPHABET, 'a')]:
+            assert pattern in patterns
+
     def test_generate_images(self):
         """
         """
         datasets_generator = d.create_datasets_generator()
-        assert len(datasets_generator.generate_images()) == 16425
+        assert len(datasets_generator.generate_images()) == 17100
+
+
+    def test_calc_positions(self):
+        """_calc_positions
+        """
+        datasets_generator = d.create_datasets_generator()
+        positions = datasets_generator._calc_positions(17)
+        assert len(positions) == 25
+        for position in [(3.5, 3.5), (6.5, 5.5), (5.5, 5.5), (7.5, 7.5)]:
+            assert position in positions
